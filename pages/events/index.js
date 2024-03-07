e06b5235bd76ee1b35f96018bca85eb8faf9ac84
Ayush Saxena <ayush.saxena2019@vitstudent.ac.in>
2024-03-07 11:25:44 +0530

    Adding functionality to list all events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,10 @@
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
 export default function AllEventsPage() {
+  const events = getAllEvents();
   return (
     <div>
-      <h1>Events Page: List all events</h1>
+      <EventList items={events} />
     </div>
   );
 }
