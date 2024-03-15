b7f8d1a9e5a7b68df3d75c5550aa3066a96b9cf8
Ayush Saxena <ayush.saxena2019@vitstudent.ac.in>
2024-03-15 12:41:54 +0530

    Added static generation to dynamic pages as well(getStaticPaths)

--- helpers/api-util.js
+++ helpers/api-util.js
@@ -1,21 +1,42 @@
 export async function getAllEvents() {
   const response = await fetch(
-    "https://nextjs-course-c81cc-default-rtdb.firebaseio.com/events.json"
+    "https://nextjs-course-33cfb-default-rtdb.firebaseio.com/events.json"
   );
-  const data = response.json();
+  const data = await response.json();
 
   const events = [];
+
   for (const key in data) {
     events.push({
       id: key,
       ...data[key],
-      // Basically we are trying to copy everything from the object as it is.
     });
   }
 
   return events;
 }
+
 export async function getFeaturedEvents() {
   const allEvents = await getAllEvents();
   return allEvents.filter((event) => event.isFeatured);
+}
+
+export async function getEventById(id) {
+  const allEvents = await getAllEvents();
+  return allEvents.find((event) => event.id === id);
+}
+
+export async function getFilteredEvents(dateFilter) {
+  const { year, month } = dateFilter;
+
+  const allEvents = await getAllEvents();
+
+  let filteredEvents = allEvents.filter((event) => {
+    const eventDate = new Date(event.date);
+    return (
+      eventDate.getFullYear() === year && eventDate.getMonth() === month - 1
+    );
+  });
+
+  return filteredEvents;
 }
