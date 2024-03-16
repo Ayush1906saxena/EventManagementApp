43d37fbe3a0888c16623d40bff1057068e1600f3
Ayush Saxena <ayush.saxena2019@vitstudent.ac.in>
2024-03-16 19:23:03 +0530

    Added server side rendering to props page.

--- helpers/api-util.js
+++ helpers/api-util.js
@@ -23,7 +23,7 @@ export async function getFeaturedEvents() {
 
 export async function getEventById(id) {
   const allEvents = await getAllEvents();
-  return allEvents.find((event) => event.id === id);
+  return allEvents.filter((event) => event.id === id);
 }
 
 export async function getFilteredEvents(dateFilter) {
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,12 @@
-import { getAllEvents } from "../../dummy-data";
-import { Fragment } from "react";
+import { getAllEvents } from "../../helpers/api-util";
+import { Fragment, use } from "react";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
 import { useRouter } from "next/router";
 
-export default function AllEventsPage() {
-  const events = getAllEvents();
+export default function AllEventsPage(props) {
   const router = useRouter();
+  const { events } = props;
 
   function findEventsHandler(year, month) {
     const fullPath = `/events/${year}/${month}`;
@@ -21,4 +21,15 @@ export default function AllEventsPage() {
       <EventList items={events} />
     </Fragment>
   );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events: events,
+    },
+    revalidate: 60,
+  };
 }
